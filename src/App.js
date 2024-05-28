66810850d67d0e36741984c1e042081a8eb3ddcb
YunusBayraktar <123024235+YunusBayraktar@users.noreply.github.com>
2024-05-28 23:33:18 +0300

    simple clock

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React , {useState} from 'react';
 import './App.css';
 
 function App() {
+let time = new Date().toLocaleTimeString();
+const [currentTime,setCurrentTime] = useState(time);
+
+  function Time () {
+  let time = new Date().toLocaleTimeString();
+  setCurrentTime(time);
+}
+
+setInterval(Time,1000);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{currentTime}</h1>
     </div>
   );
 }
